957bbf94f3e6eb0a6b76cb1055f492cbae53c4eb
MAOMAOFAN9 <akasakaryuunosuke93@gmail.com>
2025-11-18 12:01:33 +0700

    Fix_Module

---
+++ Modul7/Unguided/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+int main() {
+    Stack S;
+
+    // latihan 1
+    cout << "Hello world!" << endl;
+    createStack(S);
+    push(S, 3);
+    push(S, 4);
+    push(S, 8);
+    pop(S);
+    push(S, 2);
+    push(S, 3);
+    pop(S);
+    push(S, 9);
+
+    printInfo(S);
+    cout << "balik stack" << endl;
+    balikStack(S);
+    printInfo(S);
+    cout << endl;
+
+    // latihan 2
+    cout << "Hello world!" << endl;
+    createStack(S);
+    pushAscending(S, 3);
+    pushAscending(S, 4);
+    pushAscending(S, 8);
+    pushAscending(S, 2);
+    pushAscending(S, 3);
+    pushAscending(S, 9);
+
+    printInfo(S);
+    cout << "balik stack" << endl;
+    balikStack(S);
+    printInfo(S);
+    cout << endl;
+
+    // latihan 3
+    cout << "Hello world!" << endl;
+    createStack(S);
+    getInputStream(S);
+    printInfo(S);
+
+    cout << "balik stack" << endl;
+    balikStack(S);
+    printInfo(S);
+
+    return 0;
+}
